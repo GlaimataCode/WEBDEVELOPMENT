d8605bd4d08f1ef3d23982b4204f82e0d30ab791
GlaimataCode <151416502+GlaimataCode@users.noreply.github.com>
2024-01-14 16:05:19 +0900

    update day 12 Style
    Kria style ba Login

--- day12/login.php
+++ day12/login.php
@@ -13,24 +13,29 @@
     </div>
     <div class="container">
         <div class="row justify-content-center text-center m-5">
-             <div class="col-md-4 border p-5"> <!--bele aumenta css tuir nesesidade presiza -->
+             <div class="col-md-4 border p-5" style="background-color:azure;"> <!--bele aumenta css tuir nesesidade presiza -->
 
                 <h3>Formulariu Login</h3>
                 <hr>
 
-                <div class="m-3">
-                    <label for="username" class="form-label">Username:</label>
-                    <input type="text" class="form-control" name="username" id="username" placeholder="Hatama Username">
-                </div>
-                <div class="m-3">
-                    <label for="password" class="form-label">Password:</label>
-                    <input type="text" class="form-control" name="password" id="password" placeholder="hatama Password">
-                </div>
-                <div class="m-3">
-                    <button class="btn btn-primary" type="submit">Login</button>
-                </div>
+               <form action="login_control.php" method="post">
+                    <div class="m-3">
+                        <label for="username" class="form-label">Username:</label>
+                        <input type="text" class="form-control" name="username" id="username" placeholder="Hatama Username">
+                    </div>
+                    <div class="m-3">
+                        <label for="password" class="form-label">Password:</label>
+                        <input type="text" class="form-control" name="password" id="password" placeholder="hatama Password">
+                    </div>
+                    <div class="m-3">
+                        <button class="btn btn-primary" type="submit">Login</button>
+                    </div>
+               </form>
             </div>
         </div>
     </div>
+    <footer class="bg-secondary text-white p-2 fixed-botton text-center">
+        <p>&copy;SIE @<?= date('Y')?> | ETI-DILI</p>
+    </footer>
 </body>
 </html>
